031ce5fe065c81b8a27486cb52315c5e164c3835
Zineb112 <zbelassiri@gmail.con>
2023-09-16 12:17:38 -0700

    test_base_model_dict.py addedd

--- models/base_model.py
+++ models/base_model.py
@@ -1,44 +1,53 @@
 #!/usr/bin/python3
-"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """Base class for all models"""
     def __init__(self, *args, **kwargs):
-        """Initialize id with a unique UUID converted to string"""
-        if not kwargs:
-            from models import storage
+        """ Constructor for BaseModel """
+        if kwargs:
+            """ If kwargs (dictionary representation)
+                is provided, initialize attributes from it """
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    """ Skip setting '__class__' attribute from kwargs """
+                    if key == 'created_at' or key == 'updated_at':
+                        """ Convert 'created_at' and 'updated_at'
+                            strings to datetime objects """
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+            if 'id' not in kwargs:
+                """ Generate a unique id if not provided in kwargs """
+                self.id = str(uuid.uuid4())
+            if 'created_at' not in kwargs:
+                """ Set 'created_at' to the current
+                    datetime if not provided in kwargs """
+                self.created_at = datetime.now()
+            if 'updated_at' not in kwargs:
+                """ Set 'updated_at' to the current
+                    datetime if not provided in kwargs """
+                self.updated_at = datetime.now()
+        else:
+            """ If kwargs is empty, create a new
+                instance with random id and current datetime """
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            storage.new(self)
-        else:
-            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                     '%Y-%m-%dT%H:%M:%S.%f')
-            del kwargs['__class__']
-            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """Define a string representation for the object"""
-        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
-        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+        """ Define a string representation for the object """
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update the updated_at attribute with the current datetime"""
-        from models import storage
+        """ Update the updated_at attribute with the current datetime """
         self.updated_at = datetime.now()
-        storage.save()
 
     def to_dict(self):
-        """Create a dictionary representation of the object"""
-        dictionary = {}
-        dictionary.update(self.__dict__)
-        dictionary.update({'__class__':
-                          (str(type(self)).split('.')[-1]).split('\'')[0]})
-        dictionary['created_at'] = self.created_at.isoformat()
-        dictionary['updated_at'] = self.updated_at.isoformat()
-        return dictionary
+        """ Create a dictionary representation of the object """
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        obj_dict['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        return obj_dict
