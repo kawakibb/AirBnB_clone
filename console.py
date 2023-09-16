79b23724a44e76c6118ac9ef77113f56dbc8c6e8
Zineb112 <zbelassiri@gmail.con>
2023-09-16 10:27:32 -0700

    pycodestyle fixed

--- console.py
+++ console.py
@@ -5,6 +5,8 @@ import cmd  # Import the 'cmd' module to create an interactive console
 import sys  # Import the 'sys' module to handle command-line arguments
 
 # Define the RbnbConsole class, which inherits from cmd.Cmd
+
+
 class RbnbConsole(cmd.Cmd):
     prompt = "(hbnb) "  # Set the prompt (command prompt) for the console
 
@@ -17,7 +19,8 @@ class RbnbConsole(cmd.Cmd):
 
     # Method for the 'EOF' (End of File) command
     def do_EOF(self, arg):
-        """Exit the console gracefully when the EOF command (Ctrl+D) is entered."""
+        """Exit the console gracefully when
+        the EOF command (Ctrl+D) is entered."""
         print("\nExiting the console.")
         return True
 
@@ -27,10 +30,14 @@ class RbnbConsole(cmd.Cmd):
         return True
 
 # Function to run the console in interactive mode
+
+
 def run_interactive():
     RbnbConsole().cmdloop()
 
 # Function to run the console in non-interactive mode with an input file
+
+
 def run_non_interactive(input_file):
     console = RbnbConsole()
     console.use_rawinput = False  # Disable raw input (non-interactive mode)
@@ -41,11 +48,16 @@ def run_non_interactive(input_file):
             console.onecmd(line.strip())  # Process each line as a command
 
 # Entry point of the program
+
+
 if __name__ == '__main__':
     if len(sys.argv) == 1:
-        run_interactive()  # If no arguments are provided, run the console in interactive mode
+        # If no arguments are provided, run the console in intera
+        run_interactive()
     elif len(sys.argv) == 2:
-        run_non_interactive(sys.argv[1])  # If one argument (input file name) is provided, run in non-interactive mode
+        # If one argument (input file name) is provided,
+        # run in non-interactive mode
+        run_non_interactive(sys.argv[1])
     else:
         # If more than one argument is provided, display a usage message
         print("Usage: python console.py [input_file]")
