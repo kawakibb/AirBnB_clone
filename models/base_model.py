e2fdffac6410a30d0bbeb5ba6dbd60d5483f622f
Zineb112 <zbelassiri@gmail.con>
2023-09-16 12:07:34 -0700

    test_base_model.py & base_model.py updates

--- models/base_model.py
+++ models/base_model.py
@@ -1,31 +1,44 @@
 #!/usr/bin/python3
+"""This module defines a base class for all models in our hbnb clone"""
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
-    def __init__(self):
-        # Initialize id with a unique UUID converted to string
-        self.id = str(uuid.uuid4())
-        # Initialize created_at with the current datetime
-        self.created_at = datetime.now()
-        # Initialize updated_at with the current datetime
-        self.updated_at = datetime.now()
+    """Base class for all models"""
+    def __init__(self, *args, **kwargs):
+        """Initialize id with a unique UUID converted to string"""
+        if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        # Define a string representation for the object
-        return "[{}] ({}) {}".format(
-            self.__class__.__name__, self.id, self.__dict__)
+        """Define a string representation for the object"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
 
     def save(self):
-        # Update the updated_at attribute with the current datetime
+        """Update the updated_at attribute with the current datetime"""
+        from models import storage
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
-        # Create a dictionary representation of the object
-        obj_dict = self.__dict__.copy()
-        # Add the __class__ attribute with the class name
-        obj_dict['__class__'] = self.__class__.__name__
-        # Convert created_at and updated_at to ISO format strings
-        obj_dict['created_at'] = self.created_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        obj_dict['updated_at'] = self.updated_at.strftime('%Y-%m-%dT%H:%M:%S.%f')
-        return obj_dict
+        """Create a dictionary representation of the object"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
