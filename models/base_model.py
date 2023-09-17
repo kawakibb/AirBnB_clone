815b464ded3904ad2bb781218fcb5eca11c3e813
Zineb112 <zbelassiri@gmail.con>
2023-09-17 14:37:34 -0700

    5. Store first object

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,18 @@
-#!/usr/bin/python3
-import uuid
-from datetime import datetime
 from models import storage
 
-
 class BaseModel:
+    def save(self):
+        storage.new(self)
+        storage.save()
+
     def __init__(self, *args, **kwargs):
-        """ Constructor for BaseModel """
         if kwargs:
             for key, value in kwargs.items():
                 if key != "__class__":
-                    if key == "created_at" or key == "updated_at":
-                        setattr(self, key, datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f'))
-                    else:
-                        setattr(self, key, value)
-            if "id" not in kwargs:
-                self.id = str(uuid.uuid4())
-            if "created_at" not in kwargs:
-                self.created_at = datetime.now()
-            if "updated_at" not in kwargs:
-                self.updated_at = datetime.now()
+                    setattr(self, key, value)
+            self.created_at = datetime.strptime(self.created_at, "%Y-%m-%dT%H:%M:%S.%f")
+            self.updated_at = datetime.strptime(self.updated_at, "%Y-%m-%dT%H:%M:%S.%f")
         else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-
-    def __str__(self):
-        """ Define a string representation for the object """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-
-    def save(self):
-        """ Update the updated_at attribute with the current datetime """
-        self.updated_at = datetime.now()
-        """ Call save() method of storage to 
-            save the object to the JSON file """
-        storage.save()
-
-    def to_dict(self):
-        """ Create a dictionary representation of the object """
-        obj_dict = self.__dict__.copy()
-        obj_dict["__class__"] = self.__class__.__name__
-        obj_dict["created_at"] = self.created_at.isoformat()
-        obj_dict["updated_at"] = self.updated_at.isoformat()
-        return obj_dict
+            self.id = str(uuid4())
+            self.created_at = self.updated_at = datetime.now()
+            storage.new(self)
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,41 +1,32 @@
-#!/usr/bin/python3
 import json
-from os.path import exists
-from datetime import datetime
 
 class FileStorage:
-    """ Path to the JSON file """
     __file_path = "file.json"
-    """ Dictionary to store objects by class name and id """
     __objects = {}
 
     def all(self):
-        """ Return the dictionary of objects """
         return self.__objects
 
     def new(self, obj):
-        """ Set a new object in the dictionary with key: 
-            <class name>.id """
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        key = f"{obj.__class__.__name__}.{obj.id}"
         self.__objects[key] = obj
 
     def save(self):
-        """ Serialize __objects to JSON and save it to the file """
-        obj_dict = {}
+        data = {}
         for key, obj in self.__objects.items():
-            obj_dict[key] = obj.to_dict()
-        with open(self.__file_path, "w", encoding="utf-8") as file:
-            json.dump(obj_dict, file)
+            data[key] = obj.to_dict()
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
+            json.dump(data, file)
 
     def reload(self):
-        """ Deserialize JSON from the file 
-            to __objects (if the file exists) """
-        if exists(self.__file_path):
-            with open(self.__file_path, "r", encoding="utf-8") as file:
-                obj_dict = json.load(file)
-            for key, obj_data in obj_dict.items():
-                class_name, obj_id = key.split(".")
-                cls = class_name
-                obj_data["created_at"] = datetime.strptime(obj_data["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
-                obj_data["updated_at"] = datetime.strptime(obj_data["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
-                self.__objects[key] = eval(cls)(**obj_data)
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
+                data = json.load(file)
+                from models.base_model import BaseModel
+                for key, value in data.items():
+                    cls_name, obj_id = key.split('.')
+                    cls = eval(cls_name)
+                    obj = cls(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
