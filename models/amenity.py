b1a01ea0ab3d823b9d80143b922792116ee7cb35
Zineb112 <zbelassiri@gmail.con>
2023-09-19 12:58:33 -0700

    models/amenity.py added

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,4 @@
-#!/usr/bin/python3
-
-""" Amenity class."""
 from models.base_model import BaseModel
 
-
 class Amenity(BaseModel):
-    """Represent an amenity.
-    The name of the amenity.
-    """
-
     name = ""
