bb35b31bf6081363ed85b8e7257735252cb1cbc6
Zineb112 <zbelassiri@gmail.con>
2023-09-19 12:57:35 -0700

    models/city.py added

--- models/city.py
+++ models/city.py
@@ -1,12 +1,5 @@
-#!/usr/bin/python3
-"""City class."""
 from models.base_model import BaseModel
 
-
 class City(BaseModel):
-    """Represent a city.
-    The name of the city.
-    """
-
     state_id = ""
     name = ""
