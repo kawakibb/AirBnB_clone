ce8b9cd4a101fc573398aaa64eb9d17fc69a1ec3
Zineb112 <zbelassiri@gmail.con>
2023-09-19 13:12:22 -0700

    models/engine/file_storage.py added

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,45 +1,46 @@
-import json
-import os
+#!/usr/bin/python3
+
 from models.base_model import BaseModel
+from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
+from models.amenity import Amenity
 from models.review import Review
+import json
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
-    def all(self, cls=None):
-        if cls is None:
-            return self.__objects
-        return {key: value for key, value in self.__objects.items() if isinstance(value, cls)}
+    def all(self):
+        return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[key] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
+
+#key = f"{obj.__class__.__name__}.{obj.id}"
+#self.__objects[key] = obj
 
     def save(self):
         data = {}
         for key, obj in self.__objects.items():
             data[key] = obj.to_dict()
-        with open(self.__file_path, mode="w", encoding="utf-8") as file:
+        with open(self.__file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file)
 
     def reload(self):
-        if os.path.exists(self.__file_path):
-            with open(self.__file_path, mode="r", encoding="utf-8") as file:
+        try:
+            with open(self.__file_path, 'r', encoding='utf-8') as file:
                 data = json.load(file)
+                from models.base_model import BaseModel
                 for key, value in data.items():
-                    cls_name, obj_id = key.split(".")
+                    cls_name, obj_id = key.split('.')
                     cls = eval(cls_name)
-                    self.__objects[key] = cls(**value)
-        else:
-            return
-
-    def close(self):
-        self.reload()
-
-if __name__ == "__main__":
-    pass
+                    obj = cls(**value)
+                    self.__objects[key] = obj
+        except FileNotFoundError:
+            pass
